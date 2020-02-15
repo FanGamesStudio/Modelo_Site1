ec6c911ebc9337909cd32cde0d593f04d6b57b3c
Daniel Resende <daniel_dsr135@hotmail.com>
2020-02-15 21:58:17 -0200

    --

---
+++ app/views/footer.php
@@ -0,0 +1,21 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2"><a href="#"><img id="logo" src="app/img/logo.svg"></a></div>
+			<div class="col-md-1 lista_footer"><a href="?i=sobre">Sobre</a></div>
+			<div class="col-md-1 lista_footer"><a href="?i=navegar">Navegar</a></div>
+			<div class="col-md-1 lista_footer"><a href="?i=blast">Buscar</a></div>
+			<div class="col-md-1 lista_footer"><a href="#">Ajuda</a></div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<p>© 2020 Games | Todos os direitos reservados
+			</div>
+		</div>
+	</div>
+	<!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="app/js/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
---
+++ app/views/sobre.php
@@ -0,0 +1,18 @@
+<div id="main" class="center">
+	<span style="color:#9bbd46">- SOBRE -</span>
+	<h2></h2>
+</div>
+
+<div id="sobre">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<img src="app/img/j4.jpg" class="img-thumbnail img-responsive center" >
+				<p></p>
+				<p></p>
+				<p></p>
+				<p style="text-align:center"><a href="?i=navegar" class="btn btn-primary btn-lg">Conheça todas as sequências</a></p>
+			</div>
+		</div>
+	</div>
+</div>
